f3e54e3f5d23c24e02c8bff625d856c467fa3989
tomosimeonov <tomosimeonov@yahoo.com>
2013-11-14 17:26:24 -0800

    Testing SchemaQueryAPI

--- test/SchemaQueryAPITests.js
+++ test/SchemaQueryAPITests.js
@@ -62,7 +62,7 @@ exports.schemaTransformation = {
 			test.done();
 		});
 	},
-	
+
 	shouldCrashWhenNoSchemaIsProvided : function(test) {
 		schemaQueryAPi.permenentSchemaCreateror(undefined, function(err, schema) {
 			test.notEqual(err, undefined, "Should throw error if schema is not defined.");
@@ -84,21 +84,169 @@ exports.schemaTransformation = {
 			test.done();
 		});
 	},
-	
+
 	shouldNotCrashWhenThereAreNoProperties : function(test) {
 		var tempInitSchema = extend([], initialSchema);
-		tempInitSchema.definitions = undefined;
+		tempInitSchema.definitions = [];
 
 		var tempRealSchema = extend([], realSchema);
-		tempRealSchema.properties = undefined;
-		tempRealSchema.primaryKey = undefined;
-		
+		tempRealSchema.properties = [];
+		delete tempRealSchema.primaryKey;
+
 		schemaQueryAPi.permenentSchemaCreateror(tempInitSchema, function(err, schema) {
 			test.equal(err, undefined, "Should not throw error about missing properties.");
 			test.deepEqual(tempRealSchema, schema, "Should produce schema without properties.");
 			test.done();
 		});
+	},
+
+	shouldCrashWhenThereIsNoPropertiesElement : function(test) {
+		var tempInitSchema = extend([], initialSchema);
+		delete tempInitSchema.definitions;
+
+		schemaQueryAPi.permenentSchemaCreateror(tempInitSchema, function(err, schema) {
+			test.notEqual(err, undefined, "Should throw error about missing properties.");
+			test.equal(undefined, schema, "Should not produce schema without properties.");
+			test.done();
+		});
 	}
+};
+
+exports.schemaValidation = {
+	shouldReturnErrorWhenSchemaDoesNotHaveNamespace : function(test) {
+		var tempRealSchema = extend([], realSchema);
+		tempRealSchema.table = undefined;
+
+		schemaQueryAPi.isSchemaStructureCorrect(tempRealSchema, function(err, correct) {
+			test.deepEqual(err, new Error(("No namespace provided.")), "Should throw error about missing namespace.");
+			test.equal(false, correct, "Should not validate schema.");
+
+			tempRealSchema.table = "";
+			schemaQueryAPi.isSchemaStructureCorrect(tempRealSchema, function(err, correct) {
+				test.deepEqual(err, new Error(("No schema provided.")), "Should throw error about missing namespace.");
+				test.equal(false, correct, "Should not validate schema.");
+				test.done();
+			});
+		});
+	},
+
+	shouldReturnErrorWhenSchemaDoesNotHaveProperties : function(test) {
+		var tempRealSchema = extend([], realSchema);
+		tempRealSchema.properties = undefined;
+
+		schemaQueryAPi.isSchemaStructureCorrect(tempRealSchema,
+				function(err, correct) {
+					test.deepEqual(err, new Error(("No properties provided.")),
+							"Should throw error about missing properties.");
+					test.equal(false, correct, "Should not validate schema.");
+
+					tempRealSchema.properties = [];
+					schemaQueryAPi.isSchemaStructureCorrect(tempRealSchema, function(err, correct) {
+						test.deepEqual(err, new Error(("No properties provided.")),
+								"Should throw error about missing properties.");
+						test.equal(false, correct, "Should not validate schema.");
+						test.done();
+					});
+				});
+	},
+
+	shouldReturnErrorWhenSchemaDoesHavePropertiesWithWrongFormat : function(test) {
+		var tempRealSchema = extend([], realSchema);
+		tempRealSchema.properties.wrong = {};
+		schemaQueryAPi.isSchemaStructureCorrect(tempRealSchema,
+				function(err, correct) {
+					test.deepEqual(err, new Error("Property with wrong format."),
+							"Should throw error about wrong properties.");
+					test.equal(false, correct, "Should not validate schema.");
+					delete tempRealSchema.properties.wrong;
+					test.done();
+				});
+	},
+
+	shouldReturnErrorWhenSchemaDoesNotHavePrimaryKey : function(test) {
+		var tempRealSchema = extend([], realSchema);
+		tempRealSchema.primaryKey = undefined;
+		schemaQueryAPi.isSchemaStructureCorrect(tempRealSchema, function(err, correct) {
+
+			test.deepEqual(err, new Error("No primary key or primary key not specified as property"),
+					"Should throw error about missing primary key.");
+			test.equal(false, correct, "Should not validate schema.");
+
+			tempRealSchema.primaryKey = "";
+			schemaQueryAPi.isSchemaStructureCorrect(tempRealSchema, function(err, correct) {
+
+				test.deepEqual(err, new Error("No primary key or primary key not specified as property"),
+						"Should throw error about missing  primary key.");
+				test.equal(false, correct, "Should not validate schema.");
+
+				tempRealSchema.primaryKey = "no_id";
+				schemaQueryAPi.isSchemaStructureCorrect(tempRealSchema, function(err, correct) {
+
+					test.deepEqual(err, new Error("No primary key or primary key not specified as property"),
+							"Should throw error about missing  primary key.");
+					test.equal(false, correct, "Should not validate schema.");
+					test.done();
+				});
+			});
+		});
+	},
+
+	shouldReturnErrorWhenSchemaDoesNotHaveIndex : function(test) {
+		var tempRealSchema = extend([], realSchema);
+		delete tempRealSchema.indexes;
+		schemaQueryAPi.isSchemaStructureCorrect(tempRealSchema, function(err, correct) {
+			test.deepEqual(err, new Error("Index element should be present even if there are no indexes"),
+					"Should throw error about missing indexes.");
+			test.equal(false, correct, "Should not validate schema.");
+			test.done();
+		});
+	},
+
+	shouldValidateSchemaWhenItIsCorrect : function(test) {
+		schemaQueryAPi.isSchemaStructureCorrect(realSchema, function(err, correct) {
+			test.equal(err, undefined, "Should not throw error on correct schema.");
+			test.equal(true, correct, "Should validate schema.");
+			test.done();
+		});
+	}
+};
+
+exports.isSchemaValid = {
+
+	shouldValidatePropertiesAsSchemaComplingWhenTheyAreInTheSchema : function(test) {
+		var properties = [ 'id', 'fname' ];
+		schemaQueryAPi.isSchemaComplied(properties, realSchema, function(err, correct) {
+			test.equal(err, undefined, "Should not throw error when is schema compling.");
+			test.equal(true, correct, "Properties are correct, should match");
+			test.done();
+		});
+	},
 	
+	shouldNotNValidatePropertiesAsSchemaComplingWhenThereIsPropNotInSchema : function(test) {
+		var properties = [ 'id', 'fname1' ];
+		schemaQueryAPi.isSchemaComplied(properties, realSchema, function(err, correct) {
+			test.equal(err, undefined, "Should not throw error when is schema compling.");
+			test.equal(false, correct, "Properties are correct, should match");
+			test.done();
+		});
+	},
 	
+	shouldProduceAnErrorWhenFormatIsNotAsExpected : function(test) {
+		var properties = undefined;
+		schemaQueryAPi.isSchemaComplied(properties, realSchema, function(err, correct) {
+			test.notEqual(err, undefined, "Should throw error when when provide undentified as props");
+			//TODO Should be false
+			test.equal(true, correct, "Properties are correct, should match");
+			
+			properties = [];
+			var tempRealSchema = extend([], realSchema);
+			delete tempRealSchema.properties;
+			schemaQueryAPi.isSchemaComplied(properties, tempRealSchema, function(err, correct) {
+				test.notEqual(err, undefined, "Should throw error when schema wrong format");
+				//TODO Should be false
+				test.equal(true, correct, "Properties are not correct, should not match");
+				test.done();
+			});
+		});
+	}
 };
