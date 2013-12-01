ac2ebd5b5f47024ab3e6ae3ff2aef69e02f8a4b3
tomosimeonov <tomosimeonov@yahoo.com>
2013-12-01 13:19:30 +0000

    Adding aggregation support and testing

--- test/SimpleQueryExecutorTests.js
+++ test/SimpleQueryExecutorTests.js
@@ -13,6 +13,9 @@ var data = [ {
 }, {
 	'id' : 3,
 	'num' : 3
+}, {
+	'id' : 4,
+	'num' : 2
 } ];
 
 var mockLScan = function(namespace) {
@@ -25,7 +28,7 @@ var filterPlan = {
 	'right' : 2
 };
 
-var props = ['*'];
+var props = [ '*' ];
 
 var namespace = 'noneed';
 
@@ -40,7 +43,7 @@ exports.simpleTests = {
 			'right' : 5
 		};
 
-		queryExecutor.registerQuery(undefined, 1, function(err,data) {
+		queryExecutor.registerNewQuery(undefined, 1, function(err, data) {
 			test.equal(0, data.length, 'Should not match data.');
 			test.done();
 		}, function(err, id) {
@@ -54,29 +57,57 @@ exports.simpleTests = {
 			'right' : 2
 		};
 
-		queryExecutor.registerQuery(undefined, 1, function(err,data) {
-			test.equal(1, data.length, 'Should return one.');
-			test.equal(4,data[0]['num'], 'Should return num equal to 4');
+		queryExecutor.registerNewQuery(undefined, 1, function(err, data) {
+			test.equal(1, data.length, 'Should return two.');
+			test.equal(4, data[0]['num'], 'Should return num equal to 4');
 			test.done();
 		}, function(err, id) {
 			queryExecutor.updateWithLocalMatch(id, props, namespace, filterPlan);
 		});
 	},
-	
+
 	shouldReturnDataOnObjectCountStopper : function(test) {
 		var filterPlan = {
 			'operator' : '=',
 			'left' : 'num',
 			'right' : 3
 		};
-
-		queryExecutor.registerQuery(2, undefined, function(err,data) {
-			test.equal(2, data.length, 'Should return one.');
-			test.notEqual(2,data[0]['id'], 'Should not have id 2');
-			test.notEqual(2,data[1]['id'], 'Should not have id 2');		
+		queryExecutor.registerNewQuery(5, undefined, function(err, data) {
+			test.equal(2, data.length, 'Should return two.');
+			test.notEqual(2, data[0]['id'], 'Should not have id 2');
+			test.notEqual(2, data[1]['id'], 'Should not have id 2');
 			test.done();
 		}, function(err, id) {
 			queryExecutor.updateWithLocalMatch(id, props, namespace, filterPlan);
+		});
+	},
+	shouldReturnDataWithAggregation : function(test) {
+		var filterPlan = {
+			'operator' : '>',
+			'left' : 'id',
+			'right' : 1
+		};
+		queryExecutor.registerNewQuery(undefined, 1, function(err, data) {
+			test.equal(1, Object.keys(data).length, 'Should return one object.');
+			test.equal(9, data[0]['SUM(num)'], 'Should sum to 9');
+			test.done();
+		}, function(err, id) {
+			queryExecutor.updateWithLocalMatch(id, ['SUM(num)' ], namespace, filterPlan);
+		});
+	},
+	shouldReturnDataWithAggregationAndOtherValues : function(test) {
+		var filterPlan = {
+			'operator' : '>',
+			'left' : 'id',
+			'right' : 1
+		};
+		queryExecutor.registerNewQuery(undefined, 1, function(err, data) {
+			test.equal(3, Object.keys(data).length, 'Should return one object.');
+			test.equal(9, data[0]['SUM(num)'], 'Should sum to 9');
+			test.equal(2, data[0]['id'], 'Should sum to 9');
+			test.done();
+		}, function(err, id) {
+			queryExecutor.updateWithLocalMatch(id, ['id','SUM(num)' ], namespace, filterPlan);
 		});
 	}
 };
