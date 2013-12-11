b8646fa10549d71f8d36e7d366d3149bd98558e7
tomosimeonov <tomosimeonov@yahoo.com>
2013-12-11 03:09:40 +0000

    Bug fixes to make it work with local scan

--- test/MockStorageAPIS.js
+++ test/MockStorageAPIS.js
@@ -4,12 +4,13 @@
 
 function MockStorageAPIS() {
 	var self = this instanceof MockStorageAPIS ? this : Object.create(MockStorageAPIS.prototype);
-	self.lscan = undefined;
+	self.Node = [];
+	self.Node.lscan= undefined;
 	return self;
 }
 
 MockStorageAPIS.prototype.setLscan = function(lscanDataFunction){
-	this.lscan = lscanDataFunction;
+	this.Node.lscan = lscanDataFunction;
 };
 
 //MockStorageAPIS.prototype.lscan = function(namespace){
