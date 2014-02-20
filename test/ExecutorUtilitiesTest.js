70ff93a0e47b40141432f60a312bace7068a27ee
tomosimeonov <tomosimeonov@yahoo.com>
2014-02-20 22:51:19 +0000

    Making the stored data tranformation better

--- test/ExecutorUtilitiesTest.js
+++ test/ExecutorUtilitiesTest.js
@@ -5,34 +5,49 @@ var utilities = require('../lib/executors/ExecutorUtilities');
 var extend = require('util')._extend;
 
 var schemaWithoutNumbers = {};
-
-schemaWithoutNumbers.id = {
+schemaWithoutNumbers.properties = [];
+schemaWithoutNumbers.properties.id = {
 	type : 'string',
 	notNull : true
 };
 
-schemaWithoutNumbers.fname = {
+schemaWithoutNumbers.properties.fname = {
 	type : 'string',
 	notNull : false
 };
-schemaWithoutNumbers.lname = {
+schemaWithoutNumbers.properties.lname = {
 	type : 'string',
 	notNull : false
 };
 
-var schemaWithNumbers = extend([], schemaWithoutNumbers);
-schemaWithNumbers.age = {
+var schemaWithNumbers = {};
+schemaWithNumbers.properties = [];
+schemaWithNumbers.properties.age = {
 	type : 'int',
 	notNull : false
 };
 
-schemaWithNumbers.salary = {
+schemaWithNumbers.properties.salary = {
 	type : 'float',
 	notNull : false
 };
-exports.transformStoredEntryToSchemaValid = {
 
-	shouldTransformEntryWithoutNumbers : function(test) {
+schemaWithNumbers.properties.id = {
+		type : 'string',
+		notNull : true
+	};
+
+schemaWithNumbers.properties.fname = {
+		type : 'string',
+		notNull : false
+	};
+schemaWithNumbers.properties.lname = {
+		type : 'string',
+		notNull : false
+	};
+exports.produceUnmarshallerDataFunction = {
+
+	shouldBeAbleToUnmarshallDataWithoutNumsOrAggreg : function(test) {
 		var storedEntry = {};
 		storedEntry.id = "a";
 		storedEntry.fname = "Tom";
@@ -43,12 +58,17 @@ exports.transformStoredEntryToSchemaValid = {
 		properEntry.fname = "Tom";
 		properEntry.lname = "Sim";
 
-		utilities.transformStoredEntryToSchemaValid(schemaWithoutNumbers, [ storedEntry ], function(entry) {
-			test.deepEqual([ properEntry ], entry, "There should be no change in the entry");
-			test.done();
+		utilities.produceUnmarshallerDataFunction(schemaWithoutNumbers, undefined, function(unmarshaller) {
+
+			unmarshaller([ storedEntry ], function(entry) {
+				test.deepEqual([ properEntry ], entry, "There should be no change in the entry");
+				test.done();
+			});
+
 		});
 	},
-	shouldTransformEntryWithNumbers : function(test) {
+
+	shouldBeAbleToUnmarshallDataWithoutAggreg : function(test) {
 		var storedEntry = {};
 		storedEntry.id = "a";
 		storedEntry.fname = "Tom";
@@ -63,9 +83,52 @@ exports.transformStoredEntryToSchemaValid = {
 		properEntry.age = 18;
 		properEntry.salary = 2.2;
 
-		utilities.transformStoredEntryToSchemaValid(schemaWithoutNumbers, [ storedEntry ], function(entry) {
-			test.deepEqual([ properEntry ], entry, "There should be change in the entry");
-			test.done();
+		utilities.produceUnmarshallerDataFunction(schemaWithNumbers, undefined, function(unmarshaller) {
+
+			unmarshaller([ storedEntry ], function(entry) {
+				test.deepEqual([ properEntry ], entry, "There should be change in the entry");
+				test.done();
+			});
+
+		});
+
+	},
+
+	shouldBeAbleToUnmarshallAllData : function(test) {
+		var storedEntry = {};
+		storedEntry.id = "a";
+		storedEntry.fname = "Tom";
+		storedEntry.lname = "Sim";
+		storedEntry.age = "18";
+		storedEntry.salary = "2.2";
+		storedEntry['COUNT(id)'] = "2";
+		storedEntry['COUNT(fname)'] = "32";
+
+		var properEntry = {};
+		properEntry.id = "a";
+		properEntry.fname = "Tom";
+		properEntry.lname = "Sim";
+		properEntry.age = 18;
+		properEntry.salary = 2.2;
+
+		properEntry['COUNT(id)'] = 2;
+		properEntry['COUNT(fname)'] = 32;
+
+		var aggregations = [ {
+			'typ' : 'COUNT',
+			'prop' : 'id'
+		}, {
+			'typ' : 'COUNT',
+			'prop' : 'fname'
+		} ];
+		// TODO
+		utilities.produceUnmarshallerDataFunction(schemaWithNumbers, aggregations, function(unmarshaller) {
+
+			unmarshaller([ storedEntry ], function(entry) {
+				test.deepEqual([ properEntry ], entry, "There should be change in the entry");
+				test.done();
+			});
+
 		});
 	}
 
