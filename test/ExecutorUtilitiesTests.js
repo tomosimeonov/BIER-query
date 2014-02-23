6cb32c348a77e3e45e97949784bc16fa3b763e80
tomosimeonov <tomosimeonov@yahoo.com>
2014-02-23 08:43:30 +0000

    Adding fragmentation for messages

--- test/ExecutorUtilitiesTests.js
+++ test/ExecutorUtilitiesTests.js
@@ -3,6 +3,7 @@
  */
 var utilities = require('../lib/executors/common/Utilities');
 var extend = require('util')._extend;
+var messageBuilderBuilder = require('../lib/builders/QueryMessageBuilder');
 
 var schemaWithoutNumbers = {};
 schemaWithoutNumbers.properties = [];
@@ -33,18 +34,18 @@ schemaWithNumbers.properties.salary = {
 };
 
 schemaWithNumbers.properties.id = {
-		type : 'string',
-		notNull : true
-	};
+	type : 'string',
+	notNull : true
+};
 
 schemaWithNumbers.properties.fname = {
-		type : 'string',
-		notNull : false
-	};
+	type : 'string',
+	notNull : false
+};
 schemaWithNumbers.properties.lname = {
-		type : 'string',
-		notNull : false
-	};
+	type : 'string',
+	notNull : false
+};
 exports.produceUnmarshallerDataFunction = {
 
 	shouldBeAbleToUnmarshallDataWithoutNumsOrAggreg : function(test) {
@@ -122,8 +123,7 @@ exports.produceUnmarshallerDataFunction = {
 			'typ' : 'COUNT',
 			'prop' : 'fname'
 		} ];
-		// TODO
-		
+
 		var tester = {};
 		tester["a"] = storedEntry;
 		utilities.produceUnmarshallerDataFunction(schemaWithNumbers, aggregations, function(unmarshaller) {
@@ -139,4 +139,68 @@ exports.produceUnmarshallerDataFunction = {
 		});
 	}
 
+};
+
+exports.messageFragmentation = {
+	shouldNotFragmentSmallMessage : function(test) {
+		var i = 0;
+		var store = {};
+		store.Node = {};
+		store.Node.send = function(data) {
+			fired = true;
+			i = i + 1;
+			test.equal(i, 1, "Should be called only once");
+		};
+
+		var payload = {};
+		payload.a = {};
+		payload.a.a = 1;
+		payload.a.b = 2;
+		payload.b = {};
+		payload.b.a = 3;
+		payload.b.b = 4;
+
+		var messageB = messageBuilderBuilder.QueryMessageBuilder().setQueryId("test").setSimpleType().setOrigin("test")
+				.setPayloadTypeData();
+
+		utilities.sendDataMessage("test", messageB, payload, store);
+		setTimeout(function() {
+			test.equal(fired, true, "Should be called"), test.done();
+		}, 1000);
+	},
+
+	shouldFragmentBigMessage : function(test) {
+		var i = 0;
+		var store = {};
+		store.Node = {};
+		store.Node.send = function(data) {
+			fired = true;
+			i = i + 1;
+			test.equal(i <= 2, true, "Should be called more than once");
+		};
+
+		var fired = false;
+		var payload = {};
+		payload.a = {};
+		payload.b = {};
+		payload.a.a = "";
+		payload.a.b = "";
+		payload.b.a = "";
+		payload.b.b = "";
+		for (var ii = 0; ii < 100000; ii++) {
+			payload.a.a = payload.a.a + "123456789101112131415161718192021222324252627282930";
+			payload.a.b = payload.a.b + "123456789101112131415161718192021222324252627282930";
+			payload.b.a = payload.b.a + "123456789101112131415161718192021222324252627282930";
+			payload.b.b = payload.b.b + "123456789101112131415161718192021222324252627282930";
+		}
+
+		var messageB = messageBuilderBuilder.QueryMessageBuilder().setQueryId("test").setSimpleType().setOrigin("test")
+				.setPayloadTypeData();
+
+		utilities.sendDataMessage("test", messageB, payload, store);
+		setTimeout(function() {
+			test.equal(i, 2, "Not correct");
+			test.equal(fired, true, "Should be called"), test.done();
+		}, 4000);
+	}
 };
